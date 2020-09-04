d4f47af6bbbafd0e229cb7187704f076f7ca19e1
Mikhail <mikhbych@gmail.com>
2020-09-05 01:01:36 +0300

    add id andpassword

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class AppConfig:
 
     PORT = 3978
-    APP_ID = ""
-    APP_PASSWORD = ""
+    APP_ID = os.environ.get("MicrosoftAppId", "0f405b83-8b6b-4f97-86e5-2a7753c73bb2")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "520x1oVD1Wo7R_MFH.V6U8j-_u85H~Hge2")
